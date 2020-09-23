d42ceffe10cdf35fcbdbd85fcb8033b70d3a6bdf
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-09-23 19:06:18 +0900

    rename app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "routine_record_production"
+  # config.active_job.queue_name_prefix = "habit_trigger_production"
 
   config.action_mailer.perform_caching = false
 
